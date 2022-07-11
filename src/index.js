1fa2783da37af5828f85962ce2e5f43faafc76ac
Jared <mumawjared@gmail.com>
2022-07-11 10:05:01 -0700

    COMMIT MSG

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ let isForm = false;
 const addBeerButton = document.getElementById('new-beer-btn');
 const formContainer = document.querySelector('.container');
 
+let beerArr = [];
+
 const fetchBeers = (callback) =>
 {
     fetch(`https://api.punkapi.com/v2/beers`)
@@ -21,6 +23,16 @@ addBeerButton.addEventListener('click', () =>
         formContainer.style.display = "block";
 });
 
+formContainer.addEventListener('submit', (event) =>
+{
+    event.preventDefault();
+    let newBeer = {};
+    newBeer.name = document.getElementById('1').value;
+    newBeer.image_url = document.getElementById('2').value;
+    newBeer.tagline = document.getElementById('3').value;
+    renderCards([newBeer]);
+})
+
 const mainDiv = document.getElementById('beer-collection');
 
 const renderCards = (beers) =>
@@ -36,19 +48,46 @@ const renderCards = (beers) =>
     div.style.display = "inline-grid"
     div.style.width = '250px'
     div.classname = 'card'
+
     btn.classname = 'like-btn'
     btn.textContent = '↑upvote↑';
+    btn.id = 'upvote'
+
     h4.innerHTML = beer.name;
     img.src = beer.image_url;
     p.innerHTML = beer.tagline;
-    btn.id = 'upvote'
+
     div.append(h4,img,p,btn);
     mainDiv.append(div);
-    console.log("LOGGING: \n")
-    console.log(beer.name + '\n')
-    console.log(beer.image_url + '\n')
     })
 }
-
+const populateArray = (data) =>
+{
+    data.forEach(beer =>{
+        beerArr.push(beer);
+    })
+    console.log(beerArr)
+}
+let beerAttributes = {abv: [], ibu: [], name: [],}
+const createObjectOfBeerAttributeValues = (array,callback) =>
+{
+    for(let index in array)
+    {
+    let beerObj = array[index]
+    for(let key in beerObj) {
+        if(key !== 'id' && key !== 'image_url'&& key !== 'description'&& key !== 'tagline')
+        {
+            let objAtkey = beerObj[key];
+            callback(objAtKey);
+        }
+        }
+    }
+}
+const attributeDropDown = () =>
+{
+    
+}
+fetchBeers(populateArray);
+createObjectOfBeerAttributeValues(beerArr)
 fetchBeers(renderCards);
 
