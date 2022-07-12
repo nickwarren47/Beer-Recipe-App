73a9ec2f3b88d34318766119a6cc8ec0964ecb3a
Nick Warren <nickwarren47@gmail.com>
2022-07-12 16:16:35 -0600

    created hamburger menu bar at the top of screen

--- src/index.js
+++ src/index.js
@@ -143,6 +143,18 @@ const detailCards = (beer) => {
   });
 };
 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
 const renderCards = (beers) => {
   const mainDiv = document.getElementById("beer-collection");
   beers.forEach((beer) => {
